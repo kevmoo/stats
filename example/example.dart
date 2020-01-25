21f704eb3984b4564a7f39d6e1ebf6a226bb8399
Kevin Moore <kevmoo@users.noreply.github.com>
2020-01-25 14:16:15 -0800

    Added rms (Root Mean Square) in stats.dart (#5)
    Root Mean Square:  square root of the mean square
    
    Fixes https://github.com/kevmoo/stats/pull/3
    
    Co-authored-by: https://github.com/Sacchid

--- example/example.dart
+++ example/example.dart
@@ -4,6 +4,6 @@ void main() {
   var input = [1, 2, 3, 4, 5, 6, 7, 8];
   print('Input: $input');
   var stats = Stats.fromData(input);
-  // {count: 8, mean: 4.5, median: 4.5, max: 8, min: 1, standardDeviation: 2.29}
+  // {count: 8, mean: 4.5, median: 4.5, max: 8, min: 1, standardDeviation: 2.29, rms: 5.05}
   print(stats.withPrecision(3));
 }
