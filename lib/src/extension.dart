0d6a9c4b9b28595b5ff3e678c6232c7d1ee72158
Kevin Moore <kevmoo@google.com>
2020-02-06 15:23:12 -0800

    Add extensions for sum and average

--- lib/src/extension.dart
+++ lib/src/extension.dart
@@ -1,5 +1,29 @@
 import 'stats.dart';
 
-extension StatsExtensions<T> on Iterable<num> {
+extension StatsExtensions on Iterable<num> {
   Stats get stats => Stats.fromData(this);
+
+  /// Returns the sum of all values in `this`.
+  num get sum {
+    num runningSum = 0;
+    for (var value in this) {
+      runningSum += value;
+    }
+
+    return runningSum;
+  }
+
+  /// Returns the average (mean) of all values in `this`.
+  ///
+  /// `this` is only enumerated once.
+  num get average {
+    var count = 0;
+    num runningSum = 0;
+    for (var value in this) {
+      count++;
+      runningSum += value;
+    }
+
+    return runningSum / count;
+  }
 }
--- test/stats_test.dart
+++ test/stats_test.dart
@@ -3,6 +3,18 @@ import 'dart:math' as math;
 import 'package:stats/stats.dart';
 import 'package:test/test.dart';
 
+Stats _validateJson(
+  Iterable<num> values,
+  num expectedSum,
+  Map<String, dynamic> expectedJson,
+) {
+  final stats = values.stats;
+  expect(stats.toJson(), expectedJson);
+  expect(values.sum, expectedSum);
+  expect(stats.average, values.average);
+  return stats;
+}
+
 void main() {
   test('empty source is not allowed', () {
     expect(
@@ -12,59 +24,76 @@ void main() {
   });
 
   test('trivial', () {
-    expect([0].stats.toJson(), {
-      'count': 1,
-      'mean': 0.0,
-      'median': 0,
-      'max': 0,
-      'min': 0,
-      'standardDeviation': 0.0,
-      'rms': 0.0
-    });
+    _validateJson(
+      [0],
+      0,
+      {
+        'count': 1,
+        'average': 0.0,
+        'median': 0,
+        'max': 0,
+        'min': 0,
+        'standardDeviation': 0.0,
+        'rms': 0.0
+      },
+    );
   });
 
   test('two simple values', () {
-    expect([0, 2].stats.toJson(), {
-      'count': 2,
-      'mean': 1.0,
-      'median': 1.0,
-      'max': 2,
-      'min': 0,
-      'standardDeviation': 1.0,
-      'rms': 1.4142135623730951
-    });
+    _validateJson(
+      [0, 2],
+      2,
+      {
+        'count': 2,
+        'average': 1.0,
+        'median': 1.0,
+        'max': 2,
+        'min': 0,
+        'standardDeviation': 1.0,
+        'rms': 1.4142135623730951
+      },
+    );
   });
 
   test('precesion', () {
-    final stats = Stats.fromData(Iterable.generate(100, math.sqrt));
-    expect(stats.toJson(), {
-      'count': 100,
-      'mean': 6.614629471031477,
-      'median': 7.035533905932738,
-      'max': 9.9498743710662,
-      'min': 0.0,
-      'standardDeviation': 2.3972227599791047,
-      'rms': 7.035623639735144
-    });
+    final stats = _validateJson(
+      Iterable.generate(100, math.sqrt),
+      661.4629471031477,
+      {
+        'count': 100,
+        'average': 6.614629471031477,
+        'median': 7.035533905932738,
+        'max': 9.9498743710662,
+        'min': 0.0,
+        'standardDeviation': 2.3972227599791047,
+        'rms': 7.035623639735144
+      },
+    );
 
-    expect(stats.withPrecision(4).toJson(), {
-      'count': 100,
-      'mean': 6.615,
-      'median': 7.036,
-      'max': 9.95,
-      'min': 0.0,
-      'standardDeviation': 2.397,
-      'rms': 7.036
-    });
+    expect(
+      stats.withPrecision(4).toJson(),
+      {
+        'count': 100,
+        'average': 6.615,
+        'median': 7.036,
+        'max': 9.95,
+        'min': 0.0,
+        'standardDeviation': 2.397,
+        'rms': 7.036
+      },
+    );
 
-    expect(stats.withPrecision(1).toJson(), {
-      'count': 100,
-      'mean': 7.0,
-      'median': 7.0,
-      'max': 10.0,
-      'min': 0.0,
-      'standardDeviation': 2.0,
-      'rms': 7.0
-    });
+    expect(
+      stats.withPrecision(1).toJson(),
+      {
+        'count': 100,
+        'average': 7.0,
+        'median': 7.0,
+        'max': 10.0,
+        'min': 0.0,
+        'standardDeviation': 2.0,
+        'rms': 7.0
+      },
+    );
   });
 }
