bb2fefaa22fc11c10acfe2f6418b3abba1e51909
Kevin Moore <kevmoo@google.com>
2020-04-14 12:03:46 -0700

    Enable and fix a couple more lints

--- example/example.dart
+++ example/example.dart
@@ -1,9 +1,18 @@
+import 'dart:convert';
+
 import 'package:stats/stats.dart';
 
 void main() {
   final input = [1, 2, 3, 4, 5, 6, 7, 8];
   print('Input: $input');
   final stats = Stats.fromData(input);
-  // {count: 8, mean: 4.5, median: 4.5, max: 8, min: 1, standardDeviation: 2.29, rms: 5.05}
-  print(stats.withPrecision(3));
+  print(const JsonEncoder.withIndent(' ').convert(stats.withPrecision(3)));
+  // {
+  //  "count": 8,
+  //  "average": 4.5,
+  //  "min": 1,
+  //  "max": 8,
+  //  "median": 4.5,
+  //  "standardDeviation": 2.29
+  // }
 }
